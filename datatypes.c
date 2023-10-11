50a4b45441a30e406369175a27b5b5e778352e87
Amiya Hidayathulla <95171763+AmiyaHidayathulla@users.noreply.github.com>
2023-10-11 22:48:46 +0530

    datatypes.c

---
+++ datatypes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+    int x;
+    double y;
+    char str[50];
+    scanf("%d",&x);
+    scanf("%lf",&y);
+    scanf(" %[^\n^]",str);//input a suitable string that matches with the string 'HackerRank'
+    printf("%d\n",i+x);
+    printf("%.1f\n",d+y);
+    printf("%s%s",s,str);
+    return 0;
+}
